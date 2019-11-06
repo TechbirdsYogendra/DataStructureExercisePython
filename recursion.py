ed7331eae21738c5929146df6c8aa402d3fb2f9f
Yogendra Singh <yogsingh6@publicisgroupe.net>
2019-11-06 17:40:27 +0530

    Added fibonacci function in recusrion.py file

--- recursion.py
+++ recursion.py
@@ -1,3 +1,4 @@
+# This funcion returns factoril of a number.
 def factorial(n):
     if n == 0 or n == 1:
         return 1
@@ -9,3 +10,20 @@ def factorial(n):
 n = 5
 fact = factorial(n)
 print(f"Factorial of {n} is {fact}.")
+
+
+# It returns nth numner in Fibonacci series.
+def fibonacci(n):
+    if n == 1 or n == 2:
+        return n-1
+    else:
+        return fibonacci(n-2) + fibonacci(n-1)
+
+
+n_1 = 10
+fibo_number = fibonacci(n_1)
+print(f"{n_1}th number in the fibonacci series is {fibo_number}.")
+
+
+
+
